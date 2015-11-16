ac61b82d4048fe41c8b4f213f68a07b3e1923a2d
Thomas Nabord <thomas.nabord@prestashop.com>
2015-11-16 16:01:40 +0100

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Packlink Pro
---
+++ translations/es.php
@@ -0,0 +1,24 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_5e10b5fd79b0b688ccac0593dc628fe8'] = 'Packlink Pro';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_6f246519918d5af20d0bf70e37fb6a1c'] = 'Packlink PRO es una innovadora plataforma Cloud que automatiza los envíos para eCommerce y pequeñas empresas.';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_cebd5bbe0ffdecc270a8a324e5a277dd'] = 'Live mode';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_ea9df7a306e2f8b5af37b67084d0c984'] = 'Utilizar el módulo en Live mode';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activar';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_b9f5c797ebbf55adccdd8539a65a0241'] = 'Desactivar';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_cffa70587159989d91e6ce586ea65c14'] = 'Entra una dirección de correo válida';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'Email';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_dc647eb65e6711e155375218212b3964'] = 'Contraseña';
+$_MODULE['<{packlinkpro}prestashop>packlinkpro_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{packlinkpro}prestashop>configure_fc93769b55d04fe9c25342dd09f23eb0'] = 'La plataforma Cloud que automatiza los envíos';
+$_MODULE['<{packlinkpro}prestashop>configure_062bfbaa60ccf6fceaa741c0c63ec439'] = 'Tus clientes podrán aprovecharse de las ventajas de los mejores servicios de transporte en todo el mundo, con envíos de tu tienda o almacén a su puerta o a un punto de conveniencia cercano.';
+$_MODULE['<{packlinkpro}prestashop>configure_ac909f377e7fcdeac56b8c8b7b43b68f'] = 'Con Packlink PRO, el vendedor podrá centrarse en su negocio sin preocuparse por la logística. Con solo un par de clics, Prestashop y Packlink PRO estarán integrados para que el vendedor pueda gestionar los pedidos, elegir el servicio que más se ajuste a las necesidades de cada envío, imprimir las etiquetas y enviar.';
+$_MODULE['<{packlinkpro}prestashop>configure_01a094b99b6bbab3e22b92e2072712ec'] = 'Tarifas preferentes desde el primer envío';
+$_MODULE['<{packlinkpro}prestashop>configure_48576c4f77cfc83dd37486ab9551dfec'] = 'Integración con Prestashop';
+$_MODULE['<{packlinkpro}prestashop>configure_2177b3971519e00a5b6dfbbd0fe331a4'] = 'Importación múltiple desde archivo CSV/XLS';
+$_MODULE['<{packlinkpro}prestashop>configure_1ddbca941251949e41f1d762fd402404'] = 'Seguimiento de paquetes en tiempo real';
+$_MODULE['<{packlinkpro}prestashop>configure_0cc9689b6430b5930a2161154373d03d'] = 'Cobertura de seguro y atención al cliente';
+$_MODULE['<{packlinkpro}prestashop>configure_7ac1f88afff36d27ee8e5f1ae9258c84'] = 'Descubre Packlink Pro';
