db7ac701462d997f0df57189ae7452b9c708d749
mEsteban <mathieu.esteban@prestashop.com>
2015-12-03 17:06:46 +0100

    Removed folders: upgrade and sql folders
    Removed functions:
    useless functions: uninstall, renderForm, getConfigForm, getConfigFormValues, postProcess, hookHeader, hookDisplayAdminOrderContentShip
    Useless content of install():

--- packlinkpro.php
+++ packlinkpro.php
@@ -57,66 +57,14 @@ class Packlinkpro extends Module
      */
     public function install()
     {
-        Configuration::updateValue('PACKLINKPRO_LIVE_MODE', false);
-
         return parent::install() &&
-            $this->registerHook('header') &&
             $this->registerHook('backOfficeHeader') &&
-            $this->registerHook('displayAdminOrderContentShip');
     }
-
-    public function uninstall()
-    {
-        Configuration::deleteByName('PACKLINKPRO_LIVE_MODE');
-
-        return parent::uninstall();
-    }
-
-    /**
-     * Load the configuration form
-     */
-    public function getContent()
-    {
-        /**
-         * If values have been submitted in the form, process.
-         */
-        if (((bool)Tools::isSubmit('submitPacklinkproModule')) == true) {
-            $this->postProcess();
-        }
-
         $this->context->smarty->assign('module_dir', $this->_path);
 
         $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
 
         return $output;
-    }
-
-    /**
-     * Create the form that will be displayed in the configuration of your module.
-     */
-    protected function renderForm()
-    {
-        $helper = new HelperForm();
-
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->module = $this;
-        $helper->default_form_language = $this->context->language->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
-
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitPacklinkproModule';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
-
-        return $helper->generateForm(array($this->getConfigForm()));
     }
 
     /**
@@ -169,40 +117,5 @@ class Packlinkpro extends Module
                 ),
             ),
         );
-    }
-
-    /**
-     * Set values for the inputs.
-     */
-    protected function getConfigFormValues()
-    {
-        return array(
-            'PACKLINKPRO_LIVE_MODE' => Configuration::get('PACKLINKPRO_LIVE_MODE', true),
-            'PACKLINKPRO_ACCOUNT_EMAIL' => Configuration::get('PACKLINKPRO_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-            'PACKLINKPRO_ACCOUNT_PASSWORD' => Configuration::get('PACKLINKPRO_ACCOUNT_PASSWORD', null),
-        );
-    }
-
-    /**
-     * Save form data.
-     */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
-
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
-        }
-    }
-
-    /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookBackOfficeHeader()
-    {
-        if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
     }
 }
